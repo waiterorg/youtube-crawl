dce12cc4d210ee8717516f025fd226c2f516197d
Abbas Karimi <iwaiterorg@gmail.com>
2022-06-13 21:10:08 +0430

    Update

--- search_videos_api.py
+++ search_videos_api.py
@@ -6,7 +6,7 @@ import googleapiclient.discovery
 # API information
 api_service_name = "youtube"
 api_version = "v3"
-DEVELOPER_KEY = 'AIzaSyA4MXaU7iQwrjj7PYrvXw-UZJNAlGD-nlU'
+DEVELOPER_KEY = 'YOUR_API_KEY'
 # API client
 youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey = DEVELOPER_KEY)
