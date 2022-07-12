75a32f7a19b57a4ddf7aeca55080773acb22abbc
Abbas Karimi <iwaiterorg@gmail.com>
2022-07-12 13:16:14 +0430

    Update youtube_config.py

--- utils/youtube_config.py
+++ utils/youtube_config.py
@@ -4,7 +4,7 @@ import googleapiclient.discovery
 # API information
 api_service_name = "youtube"
 api_version = "v3"
-DEVELOPER_KEY = "AIzaSyC7zrfF8edCHYA13B4hgYjGxF2MgABcab4"
+DEVELOPER_KEY = "YOUR_API_KEY"
 # API client
 youtube = googleapiclient.discovery.build(
     api_service_name, api_version, developerKey=DEVELOPER_KEY
